684f626bba5da86e3101ba7c784ba4fec08aa967
Aleem Pasha <aleem.pasha@jdplc.com>
2019-05-16 09:03:57 +0530

    Header and Footer updated

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,9 +1,7 @@
 <footer class="footer">
-  <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+  <div class="text-center">
+    <span class="">(A tree begins with a seed.) أول الشجرة بذرة</span>
   </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
 </body>
 </html>
--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -7,32 +7,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/main.css');?>" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
 </head>
 
 <body>
-<div id="app" class="container">
+<div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Dar Al-Qur’an</a>
+        <h1 class="mr-2">دار القرآن</h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div id="navbarNavDropdown" class="navbar-collapse collapse">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+                <!-- <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
+                </li> -->
                 <!--ACCESS MENUS FOR ADMIN-->
                 <?php if($this->session->userdata('level')==='1'):?>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Books
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a href="<?php echo site_url('admin/bookauthor');?>" class="dropdown-item" href="#">Authors</a>
+                        <a class="dropdown-item" href="#">Categories</a>
+                        <a class="dropdown-item" href="#">Type</a>
+                    </div>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Dropdown
+                      FATWA
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">The Mufti</a>
+                        <a class="dropdown-item" href="#">Categories</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Islamic program
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Program</a>
+                        <a class="dropdown-item" href="#">Categories</a>
+                        <a class="dropdown-item" href="#">Type</a>
                     </div>
                 </li>
                 <!--ACCESS MENUS FOR STAFF-->
